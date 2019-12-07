f94ec7aca0eebfb1f10d8de2bc727ee6603d57a1
wangjiajun666 <52663234+wangjiajun666@users.noreply.github.com>
2019-12-07 15:49:57 +0800

    my-react

---
+++ build/precache-manifest.6a191b3cc43182f0fd1f2726348ec701.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daba148cbd1df830a7868869a7eebaab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bcbc634b14f429340260",
+    "url": "./static/css/2.1f22840a.chunk.css"
+  },
+  {
+    "revision": "e41b3242e0e5f6fb67e3",
+    "url": "./static/css/main.ece2d4c3.chunk.css"
+  },
+  {
+    "revision": "bcbc634b14f429340260",
+    "url": "./static/js/2.68168375.chunk.js"
+  },
+  {
+    "revision": "e41b3242e0e5f6fb67e3",
+    "url": "./static/js/main.ab0155e1.chunk.js"
+  },
+  {
+    "revision": "b9a800000d2714165c1e",
+    "url": "./static/js/runtime-main.14211a33.js"
+  },
+  {
+    "revision": "9e68aea00b0e633bcb11627888b097be",
+    "url": "./static/media/bg.9e68aea0.png"
+  },
+  {
+    "revision": "06d8394c918a46df6435db9c421c50b8",
+    "url": "./static/media/iconfont.06d8394c.woff"
+  },
+  {
+    "revision": "0950d944083aae08912a124decfe9582",
+    "url": "./static/media/iconfont.0950d944.ttf"
+  },
+  {
+    "revision": "3933630fc6624c166dc57db530b7d1ae",
+    "url": "./static/media/iconfont.3933630f.eot"
+  },
+  {
+    "revision": "653d27d8cb594d70a17d0898ccb2c035",
+    "url": "./static/media/iconfont.653d27d8.svg"
+  },
+  {
+    "revision": "903756eba29c4312043768d37bcca126",
+    "url": "./static/media/iconfont.903756eb.svg"
+  },
+  {
+    "revision": "9c119a0c92bd7d4b45761d7b2b63d215",
+    "url": "./static/media/iconfont.9c119a0c.woff"
+  },
+  {
+    "revision": "a2692c9d7d6d4cbc163bf452f359d636",
+    "url": "./static/media/iconfont.a2692c9d.ttf"
+  },
+  {
+    "revision": "a2b2f31f08e21d7da89ffdd4de5a0d9b",
+    "url": "./static/media/iconfont.a2b2f31f.eot"
+  },
+  {
+    "revision": "4ee3d045f28b5047b53a0ec7861c4de2",
+    "url": "./static/media/login-bg.4ee3d045.jpg"
+  }
+]);
